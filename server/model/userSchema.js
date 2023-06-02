0886fc2316336996f11e0e79222d2240d2b85dad
Arish82 <arish123.ahamad@gmail.com>
2023-06-03 03:04:50 +0530

    login, bcrypt and jwttoken added

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,6 @@
 const mongoose=require("mongoose");
+const bcrypt=require("bcryptjs");
+const jwt=require('jsonwebtoken');
 
 const UserSchema=new mongoose.Schema({
     name: {
@@ -24,7 +26,36 @@ const UserSchema=new mongoose.Schema({
         type: String,
         require: true
     },
+    tokens: [
+        {
+            token: {
+                type: String,
+                require: true
+            }
+        }
+    ]
 }, {timestamps: true});
+
+UserSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        var salt = bcrypt.genSaltSync(12);
+        this.password = bcrypt.hashSync(this.password,salt);
+        this.cPassword = bcrypt.hashSync(this.cPassword,salt);
+    }
+    next();
+})
+
+UserSchema.methods.generateWebToken= async function(){
+    try{
+        const token = await jwt.sign({_id: this.id},process.env.SECRET_KEY);
+        this.tokens=this.tokens.concat({token: token});
+
+        await this.save()
+        return token;
+    }catch(err){
+        console.log(err);
+    }
+}
 
 const User= new mongoose.model('User', UserSchema);
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -2,6 +2,8 @@ const express=require("express")
 const router=express.Router();
 const User=require("../model/userSchema");
 require("../db/conn")
+const bcrypt = require('bcryptjs');
+const jwt=require('jsonwebtoken');
 
 router.post("/signup",async (req,res)=>{
     console.log(req.body);
@@ -32,6 +34,35 @@ router.post("/signup",async (req,res)=>{
     }
 })
 
+router.post("/login",async (req,res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(422).json({message: "Make sure that the data sent in the request contains all valid fields and values beforehand."})
+    }
+    try{
+        const userExist=await User.findOne({email});
+        if(!userExist){
+            return res.status(401).json({message: "Invalid Credentials"});
+        }
+        
+        const match=await bcrypt.compare(password, userExist.password);
+        
+        if(!match){
+            return res.status(401).json({message: "Invalid Credentials"});
+        }
+
+        const token=await userExist.generateWebToken();
+        res.cookie('jwttoken',token,{
+            httpOnly: true
+        })
+
+        res.status(201).json({message: "Login successful"});
+    
+    }catch(err){
+        console.log(err);
+    }
+
+})
 
 
 module.exports= router;
